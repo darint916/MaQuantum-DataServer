e8696589d13c5b5d894e176aa6748ce05c664510
Darin Tsai <darint916@gmail.com>
2023-06-27 17:37:08 -0700

    README + minor import + init

--- backend/core/api/controller.py
+++ backend/core/api/controller.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, url_for
-from core.api.data_schema import battery_schema, temperature_schema, speed_schema, engine_schema, solar_schema, chassis_schema, validate_data, vesc_duty_cycle_schema, vesc_stat1_schema, vesc_stat2_schema, vesc_stat3_schema, vesc_stat4_schema, vesc_stat5_schema, bms_power_schema, bms_temp_state_schema
-from core.api.db_convert import append_data, get_data_all, get_data
+# from core.api.data_schema import 
 
 from core.api.api_response import APIResponse, AddData, GetData
 
@@ -13,3 +12,9 @@ It validates the input data using the schema.
 If the data is valid, it appends it to the CSV file and returns a success response.
 If the data is invalid or could not be added to the CSV file, it returns an error response.
 """
+
+#First API Endpoint for ChilledWaterMonitor
+@api.route("/chilled-watermonitor", methods=["POST"])
+def add_chilled_watermonitor_data():
+    data = request.get_json()
+    
